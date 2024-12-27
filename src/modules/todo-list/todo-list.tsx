49622c9ea8084799ddb8f52c4b4e3ac8043f8c26
sergey_parshutich <sergey_parshutich@relabs.ru>
2024-12-27 18:00:37 +0300

    feat: add enabled

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -5,18 +5,23 @@ import { useState } from "react";
 export function TodoList() {
   const [page, setPage] = useState(1);
 
+  const [enabled, setEnable] = useState(false);
+
   const {
     data: todoItems,
     error,
-    isPending,
+    isLoading,
+    status,
+    fetchStatus,
     isPlaceholderData,
   } = useQuery({
     queryKey: ["tasks", "list", { page }],
     queryFn: (meta) => todoListApi.getTodoList({ page }, meta),
     placeholderData: keepPreviousData,
+    enabled: enabled,
   });
 
-  if (isPending) {
+  if (isLoading) {
     return <div>Loading</div>;
   }
 
@@ -27,13 +32,14 @@ export function TodoList() {
   return (
     <div className="p-5 mx-auto max-w-[1200px] mt-10">
       <h1 className="mb-5 text-3xl font-bold underline"> Todo LIst</h1>
+      <button onClick={() => setEnable((e) => !e)}>Toggle enabled</button>
 
       <div
         className={
           "flex flex-col gap-4" + (isPlaceholderData ? " opacity-50" : "")
         }
       >
-        {todoItems.data.map((todo) => (
+        {todoItems?.data.map((todo) => (
           <div className="p-3 border rounded border-slate-300" key={todo.id}>
             {todo.text}
           </div>
@@ -47,7 +53,7 @@ export function TodoList() {
           prev
         </button>
         <button
-          onClick={() => setPage((p) => Math.min(p + 1, todoItems.pages))}
+          onClick={() => setPage((p) => Math.min(p + 1, todoItems?.pages ?? 1))}
           className="p-3 border border-teal-500 rounded"
         >
           next
--- src/shared/api/query-client.ts
+++ src/shared/api/query-client.ts
@@ -3,10 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1 * 60 * 1000, // обновление кеша
-      gcTime: 10 * 60 * 1000, // очистка кеша
-      retry: 2, // кол-во попыток
-      retryDelay: 1000, // задержка между попытками
+      staleTime: 1 * 60 * 1000,
     },
   },
 });
