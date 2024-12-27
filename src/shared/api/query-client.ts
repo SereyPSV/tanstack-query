2e5a772babe9d159a9522230bdf8d161f0cc5e77
sergey_parshutich <sergey_parshutich@relabs.ru>
2024-12-27 17:12:40 +0300

    feat: stale time

--- src/shared/api/query-client.ts
+++ src/shared/api/query-client.ts
@@ -1,3 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1 * 60 * 1000, // обновление кеша
+      gcTime: 10 * 60 * 1000, // очистка кеша
+      retry: 2, // кол-во попыток
+      retryDelay: 1000, // задержка между попытками
+    },
+  },
+});
